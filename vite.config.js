5458c9bedfc70528e8539ba6642e524673673741
rstr-dev <strachana0104@gmail.com>
2025-11-17 18:29:25 -0500

    added call to update npm

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: "./src",
+  root: './', 
   build: {
-    outDir: "../dist",
+    outDir: 'dist',
   },
 });
